76b4c6c6cc80c6bae32a643e1f14a882374b7a2d
soorya-5 <83122216+soorya-5@users.noreply.github.com>
2022-07-26 22:53:46 +0530

    day 3 code

---
+++ Day3.ino
@@ -0,0 +1,16 @@
+int led = 4;
+int switchh = 2;
+
+void setup() 
+{
+  pinMode(led,OUTPUT);
+  pinMode(switchh,INPUT);
+}
+
+void loop() 
+{
+  if (digitalRead(switchh) == 0)
+    digitalWrite(led,HIGH);
+  else
+    digitalWrite(led,LOW);
+}
